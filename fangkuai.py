350a4fc8e1e1672966036444bb6aa3caa9d5e25d
billxq <178919347@qq.com>
2018-04-15 21:20:42 +0800

    fangkuai

---
+++ fangkuai.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @date: 2018/4/15 下午7:34
+# @author: Bill
+# @file: demon1
+
+for i in range(1,6):
+    for j in range(1,6):
+        print('♦',end='')
+    print()
