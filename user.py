28e6552789d7208a2c29fc4b07c45605b0af6586
billxq <178919347@qq.com>
2018-04-18 15:27:15 +0800

    user

--- user.py
+++ user.py
@@ -6,6 +6,7 @@ class User(object):
         self.first = first
         self.last = last
         self.infos = kwargs
+        self.login_attempts = 0
     def describe_user(self):
         profile = dict()
         profile['First name'] = self.first
@@ -15,7 +16,24 @@ class User(object):
         print(profile)
     def greet_user(self):
         print("Hello,{} {}".format(self.first,self.last))
-
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+    def increment_login_attempts(self):
+        self.login_attempts += 1
 user = User('Xu','Qing',Age=18,Gender='Male',Job='Teacher')
 user.describe_user()
 user.greet_user()
+print(user.login_attempts)
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+print(user.login_attempts)
+user.reset_login_attempts()
+print(user.login_attempts)
