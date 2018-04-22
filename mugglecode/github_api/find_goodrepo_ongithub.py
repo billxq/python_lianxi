4f8cada6f6d88c829771ea4babdeeb2e95e08228
xuqing <178919347@qq.com>
2018-04-22 22:18:07 +0800

    sort files

---
+++ mugglecode/github_api/find_goodrepo_ongithub.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+从 GitHub 上选出符合这些条件的项目： 
+1. 最近一周内发布的 
+2. Star 数大于 200 
+3. topic 是 crawler
+找到后发送邮件
+利用github的api接口进行查询：https://api.github.com/search/repositories?q=
+"""
+
