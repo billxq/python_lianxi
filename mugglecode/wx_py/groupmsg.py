41e208506265136162e510a6eeb906899b151637
greenfish <178919347@qq.com>
2018-04-25 00:11:14 +0800

    add multi pages

--- mugglecode/wx_py/groupmsg.py
+++ mugglecode/wx_py/groupmsg.py
@@ -5,8 +5,14 @@
 # @file: groupmsg.py
 
 
+'''
+利用微信的wxpy模块，批量发送消息
+'''
+import time
+
 from wxpy import *
 import csv
+
 
 
 def read_info():
@@ -14,5 +20,28 @@ def read_info():
     reader = csv.DictReader(f)
     return [info for info in reader]
 
-info = read_info()
-print(info)
+def make_message(raw_info):
+    temp = '{n}-同学请于{t}时间参加{s}课程，课程地址是{a}。收到请回复，谢谢!'
+    msg_list = [temp.format(n=info['姓名'],
+                            t=info['上课时间'],
+                            s=info['课程'],
+                            a=info['上课地址']) for info in raw_info]
+    return msg_list
+
+
+def send(msg_list):
+    bot = Bot()
+    for msg in msg_list:
+        friend_name = msg.split('-')[0]
+        f = bot.friends().search(friend_name)
+        if len(f) == 1:
+            f[0].send(msg)
+        else:
+            print(friend_name)
+            print('Please check this name!')
+        time.sleep(3)
+
+
+raw_info = read_info()
+msg_list = make_message(raw_info)
+send(msg_list)
