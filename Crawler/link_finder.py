198cb8ef5ec2aefd9ec1603e0779400a83260b7d
bill <178919347>
2018-05-05 13:59:43 +0800

    links_finder

--- Crawler/link_finder.py
+++ Crawler/link_finder.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding:utf-8
+
 from html.parser import HTMLParser
 from urllib import parse
 
---
+++ Crawler/spider.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+
+from urllib.request import urlopen
+from link_finder import LinkFinder
+from general import *
+
+
+class Spider(object):
+
+    # shared information
+    project_name = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+
+    def __init__(self,project_name,base_url,domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page("First_page",base_url)
+
+    # Creates directory and files for the project on first run and start the spider
+
+    @staticmethod
+    def boot():
