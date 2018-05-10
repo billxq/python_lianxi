9a8a26eb061d99632b4067bf87a9a426d6b22e66
greenfish <178919347@qq.com>
2018-05-10 16:01:02 +0800

    multi-pro

--- scrapytest/hao6v/hao6v/spiders/hao6V.py
+++ scrapytest/hao6v/hao6v/spiders/hao6V.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from hao6v.items import Hao6VItem
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spider import CrawlSpider
+from scrapy.spider import Rule
 
-class Hao6vSpider(scrapy.Spider):
+class Hao6vSpider(CrawlSpider):
     name = 'hao6V'
     allowed_domains = ['hao6v.com']
-    offset = 2
     start_urls = ['http://www.hao6v.com/gydy/']
+    pagelink = LinkExtractor(restrict_xpaths='//div[@class="listpage"]', allow=r'index_\w+.html')
+
+    rules = [
+        Rule(pagelink, callback = "parsePage", follow = True)
+    ]
+
+    def parsePage(self, response):
+        # cards = response.xpath('//ul[@class="list"]//a')
+        item = Hao6VItem()
+        for each in response.xpath('//ul[@class="list"]/li//a'):
+            if each.xpath('./text()'):
+                # item['movie_name'] = each.xpath('./text()').extract()
+                detail_url = each.xpath('./@href').extract()[0]
+                yield scrapy.Request(detail_url,callback=self.parseDetail)
+
+    def parseDetail(self,response):
 
 
-    def parse(self, response):
-        cards = response.xpath('//ul[@class="list"]//a')
-        for each in cards:
-            item = Hao6VItem()
-            if each.xpath('./@href').re('http://www.hao6v.com/\S+?/\S+?/\S+?html'):
-                item['movie_name'] = each.xpath('./text()').extract()
-                item['movie_url'] = each.xpath('./@href').re('http://www.hao6v.com/\S+?html')[0]
-                yield item
-        if self.offset <= 66:
-            self.offset += 1
-            next_url = self.start_urls[0] + f'index_{self.offset}.html'
-        yield scrapy.Request(next_url,callback=self.parse)
+
+
+
+
 
 
 
