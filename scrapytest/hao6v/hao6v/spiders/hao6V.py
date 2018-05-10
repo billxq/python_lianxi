5f3c4f196754bac2959706ff0c3047c853fff31f
bill <178919347>
2018-05-10 22:52:56 +0800

    links_finder

--- scrapytest/hao6v/hao6v/spiders/hao6V.py
+++ scrapytest/hao6v/hao6v/spiders/hao6V.py
@@ -8,8 +8,8 @@ from scrapy.spider import Rule
 class Hao6vSpider(CrawlSpider):
     name = 'hao6V'
     allowed_domains = ['hao6v.com']
-    start_urls = ['http://www.hao6v.com/gydy/']
-    pagelink = LinkExtractor(restrict_xpaths='//div[@class="listpage"]', allow=r'index_\w+.html')
+    start_urls = ['http://www.hao6v.com/dy/']
+    pagelink = LinkExtractor(restrict_xpaths='//div[@class="listpage"]//a[last()-1]', allow=r'index_\w+.html')
 
     rules = [
         Rule(pagelink, callback = "parsePage", follow = True)
@@ -17,7 +17,7 @@ class Hao6vSpider(CrawlSpider):
 
     def parsePage(self, response):
         # cards = response.xpath('//ul[@class="list"]//a')
-        item = Hao6VItem()
+
         for each in response.xpath('//ul[@class="list"]/li//a'):
             if each.xpath('./text()'):
                 # item['movie_name'] = each.xpath('./text()').extract()
@@ -25,8 +25,20 @@ class Hao6vSpider(CrawlSpider):
                 yield scrapy.Request(detail_url,callback=self.parseDetail)
 
     def parseDetail(self,response):
-
-
+        item = Hao6VItem()
+        for each in response.xpath('//div[@class="box"]'):
+            if each.xpath('.//h1/text()'):
+                item['movie_name'] = each.xpath('.//h1/text()').extract()[0]
+                if each.xpath('.//table//tr//td//a/@href').re('(magnet:.*)'):
+                    item['movie_url'] = each.xpath('.//table//tr//td//a/@href').re('(magnet:.*)')[0]
+                    yield item
+                elif each.xpath('.//table//tr//td//a/@href').re('(ed2k:.*)'):
+                    item['movie_url'] = each.xpath('.//table//tr//td//a/@href').re('(ed2k:.*)')[0]
+                    yield item
+                elif each.xpath('.//table//tr//td//a/@href'):
+                    item['movie_url'] = each.xpath('.//table//tr//td//a/@href').extract()[0]
+                else:
+                    item['movie_url'] = None
 
 
 
