41aea106fce8865c402c0245cd3e683ec69f8192
greenfish <178919347@qq.com>
2018-05-11 15:53:42 +0800

    multi-pro

--- scrapytest/meitulu/meitulu/spiders/Meitulu.py
+++ scrapytest/meitulu/meitulu/spiders/Meitulu.py
@@ -14,37 +14,44 @@ class MeituluSpider(CrawlSpider):
     allowed_domains = ['www.meitulu.com']
     start_urls = ['https://www.meitulu.com/t/toutiaonvshen/']
 
-    pagelink = LinkExtractor(restrict_xpaths='//div[@id="pages"]//a[last()]',allow=r'/\w+.html',deny=r'/\w+_\w+.html')
+    pagelink = LinkExtractor(restrict_xpaths='//div[@id="pages"]//a[last()]',allow=r'/\w+.html')
     rules = [
         Rule(pagelink, callback = "parse_picset_url", follow=True)
     ]
+
 
 
     def parse_picset_url(self, response):
         for each in response.xpath('//div[@class="boxs"]'):
             set_urls = each.xpath('./ul/li/a/@href').extract()
             for set_url in set_urls:
-                yield scrapy.Request(set_url,callback=self.parse_item)
+                yield scrapy.Request(set_url,callback=self.parse_pages)
 
 
-    def parse_item(self,response):
-        item = MeituluItem()
-        item['title'] = response.xpath('//title/text()').extract_first(default="N/A")
-        item['url'] = response.url
-
+    def parse_pages(self,response):
+        # 解析图片地址所在的详情页
         if response.xpath('//div[@id="pages"]//a[last()-1]/text()'):
+            # 详情页最大页数
             max_num = response.xpath('//div[@id="pages"]//a[last()-1]/text()').extract_first(default="N/A")
 
         page_urls = [ response.url[:-5] + '_{}.html'.format(i) for i in range(2,int(max_num)+1)]
         for page_url in page_urls:
-            yield scrapy.Request(page_url,callback=self.img_url)
-        item['image_urls'] = self.img_urls
-        yield item
+            res = scrapy.Request(page_url)
+            for img_url in res.xpath('//div[@class="content"]//img/@src').extract():
+                self.img_urls.append(img_url)
+        print(self.img_urls)
 
-    def img_url(self,response):
-        img_urls = response.xpath('//div[@class="content"]//img/@src').extract()
-        for img_url in img_urls:
-            self.img_urls.append(img_url)
+    #
+    # def parse_items(self,response):
+    #     # 获得所有图片下载连接，返回item
+    #     img_urls = response.xpath('//div[@class="content"]//img/@src').extract()
+    #     for img_url in img_urls:
+    #         self.img_urls.append(img_url)
+    #
+    #     item = MeituluItem()
+    #     item['title'] = response.xpath('//h1/text()').re(r'(.*)\s+\w+/\w+')[0]
+    #     item['image_urls'] = self.img_urls
+    #     yield item
 
 
 
